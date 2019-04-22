123da86bf6b998a2d2066957449a490c3aefaddd
Vyacheslav Lapin <vyacheslav_lapin@epam.com>
2019-04-22 18:09:46 +0500

    Initial commit

---
+++ src/main/java/ru/sapphire/courses/Table.java
@@ -0,0 +1,21 @@
+package ru.sapphire.courses;
+
+public class Table {
+
+  public static void main(String... args) {
+    System.out.println("Hello, World!");
+    //..
+    //..
+    //..
+    //..
+    //..
+    //..
+    //..
+    //..
+    //..
+    //..
+    //..
+    //..
+    //..
+  }
+}
---
+++ src/main/java/ru/sapphire/courses/pkg1/Caller.java
@@ -0,0 +1,12 @@
+package ru.sapphire.courses.pkg1;
+
+import ru.sapphire.courses.Table;
+
+public class Caller {
+
+  public static void main(String... __) {
+//    System.out.println(__[0]);
+    Table.main();
+  }
+
+}
