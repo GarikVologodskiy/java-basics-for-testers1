896bc4bb42e0a79af62b7f6ce3f86dc8d4c1c3a6
Vyacheslav Lapin <vyacheslav_lapin@epam.com>
2019-04-23 14:49:41 +0500

    arithmetics

--- src/main/java/ru/sapphire/courses/pkg1/Caller.java
+++ src/main/java/ru/sapphire/courses/pkg1/Caller.java
@@ -6,7 +6,25 @@ public class Caller {
 
   public static void main(String... __) {
 //    System.out.println(__[0]);
-    Table.main();
+//    Table.main();
+
+//    for (int i = 0; i < 10;) {
+//        i = i++;
+//        System.out.println("Hi");
+//    }
+
+    double x = 0.0 / 0.0;
+
+    System.out.println(Double.isNaN(x)); //true
+
+    System.out.println(x != x); //false
+
+    int a, b, c;
+
+    a = b = c = 5;
+
+
+
   }
 
 }
